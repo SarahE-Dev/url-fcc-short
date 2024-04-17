11dbf38782adda509a6012ffda6bf91624a56bd1
Sarah E <seatherly.prsvr@gmail.com>
2024-04-16 19:48:46 -0500

    trya

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true})
+);
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
