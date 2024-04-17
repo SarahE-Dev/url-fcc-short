536fc9ac6d72e0ab00dc1787768793402ae1118f
Sarah E <seatherly.prsvr@gmail.com>
2024-04-16 19:40:19 -0500

    try edit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const littleid = require('littleid');
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 
@@ -18,6 +19,41 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+let urlArray = []
+app.post('/api/shorturl', function(req, res) {
+    try {
+      
+      const url = req.body.url
+      let valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
+      if(!valid){
+        res.json({error: 'invalid url'})
+        return
+      }
+      let short_url = littleid()
+      let foundUrl = urlArray.find((item) => item.original_url === url)
+      if(foundUrl){
+        res.json({original_url: foundUrl.original_url, short_url: foundUrl.short_url})
+      }
+      else {
+        urlArray.push({original_url: url, short_url: short_url})
+        res.json({original_url: url, short_url: short_url})
+      }
+    } catch (error) {
+      res.json({error: 'invalid url'})
+    }
+
+})
+
+app.get('/api/shorturl/:short_url?', function(req, res) {
+  try {
+    let found = urlArray.find((item) => item.short_url === req.params.short_url)
+    if(found){
+      res.redirect(found.original_url)
+    }
+  } catch (error) {
+    res.json({error: 'invalid url'})
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
